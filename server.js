ef14928d6c60b774a6438872e883ce8de6be6306
uluk03 <ulukakylbekov03.com>
2026-04-22 16:19:08 +0600

    update project

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Сервер работает 🚀')
-})
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on all interfaces')
+  })
 
 app.listen(3000, () => {
   console.log('http://localhost:3000')
